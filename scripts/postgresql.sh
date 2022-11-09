13d1704c93dd57456e84101a1576a1d59d382ea2
Philip Nwachukwu <philip.i.nwachukwu@gmail.com>
2022-11-09 03:26:47 +0000

    Updating main.yml

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -4,11 +4,15 @@ sudo apt update
 
 sudo apt install postgresql-client-15 postgresql-15 -y
 
-sudo -u postgres psql -c "CREATE USER phil_altschool WITH PASSWORD 'p@ssw0rd';"
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'root';"
 
-sudo -u postgres psql -c "CREATE DATABASE phil_altschool;"
+#sudo -u postgres psql -c "CREATE USER phil_altschool WITH PASSWORD 'p@ssw0rd';"
 
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE phil_altschool to phil_altschool;"
+#sudo -u postgres psql -c "CREATE DATABASE phil_altschool;"
+
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE phil_altschool to phil_altschool;"
+
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO phil_altschool"
 
 sudo systemctl restart postgresql 
 
